c7e005275601f6d01d0ac3993007d002e9edbe36
Lucas Shaffer <shafferlucas@gmail.com>
2022-05-08 22:44:23 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send("<h2>It's Working!</h2>");
+})
+
+app.listen(PORT, () => {
+  console.log(`API is listening on port ${PORT}`)
+})
